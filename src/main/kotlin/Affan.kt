a901b3f10c699aef83f572ef95849b2ef055c504
Affan Zanuar <affanzanuar1@gmail.com>
2022-06-23 23:36:40 +0700

    Menambahkan fungsi jumlah array

---
+++ src/main/kotlin/Affan.kt
@@ -0,0 +1,22 @@
+class Affan {
+    companion object{
+        @JvmStatic
+        fun main(args: Array<String>) {
+
+            var a = intArrayOf(1,2,3,4,5);
+
+            var b = jumlahArray(a);
+
+            println(b)
+
+        }
+
+        fun jumlahArray(dataArray : IntArray) : Int{
+            var j = 0;
+            for ( i in dataArray){
+                j+=i
+            }
+            return j
+        }
+    }
+}
