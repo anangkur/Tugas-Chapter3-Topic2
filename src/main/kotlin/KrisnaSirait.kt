8d769881f986fc9f2567ae70af212a0336fb4131
Krisna Sirait <krisnapande00@outlook.com>
2022-06-24 22:23:13 +0700

    created fun askInfo inside KrisnaSirait class

--- src/main/kotlin/KrisnaSirait.kt
+++ src/main/kotlin/KrisnaSirait.kt
@@ -3,10 +3,35 @@ class KrisnaSirait {
         print("Masukkan nama anda : ")
         val name: String? = readLine()
         print("Masukkan usia anda : ")
-        val age:Int? = readLine()?.toInt()
+        var age:Int = readLine()?.toInt() ?: 0
+        print("Masukkan usia adik anda : ")
+        var ageBro: Int = readLine()?.toInt() ?: 0
 
-        println(name)
-        println(age)
+        //Arithmatic
+        val resultMod = (age?:0) % (ageBro?:0)
+        val resultMul = (age?:0) * (ageBro?:0)
+        val resultDiv = (age?:0) / (ageBro?:0)
+        val resultALl = resultDiv * resultDiv / resultMod
+
+        //String operator
+        val resultGreetings  = "Nama saya " + name.toString() + ", Umur saya "+ age.toString() + ", Umur adik saya "+ ageBro
+
+        //assigned operator
+        ageBro += age
+
+        //unary operator
+        print("Apakah anda keluarga? ")
+        val isFamily = readLine()?.toBoolean() ?: false
+        val resUnary = !isFamily
+
+//        println(name)
+//        println(age)
+//        println(resultMod)
+//        println(resultMul)
+//        println(resultDiv)
+//        println(resultALl)
+//        println(resultGreetings)
+        println(resUnary)
     }
 
 
